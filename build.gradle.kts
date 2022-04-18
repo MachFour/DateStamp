69c78afccdcbfaaf9477eb2776c2dbae1164f031
Max Fisher <machfour+git@protonmail.com>
2022-04-19 01:09:13 +1000

    tweak build config

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,10 +44,6 @@ kotlin {
         val commonMain by getting
         val commonTest by getting
         val jvmMain by getting
-        val jvmTest by getting
-        //val jsMain by getting
-        //val jsTest by getting
         val nativeMain by getting
-        val nativeTest by getting
     }
 }
